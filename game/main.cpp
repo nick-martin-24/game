e51d66b97ee515539846638ab8b3cd7e0a81efa8
nick-martin-24 <nsm5102@gmail.com>
2020-11-21 10:20:40 -0500

    Initial Commit

---
+++ game/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  game
+//
+//  Created by Nick Martin on 11/21/20.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
